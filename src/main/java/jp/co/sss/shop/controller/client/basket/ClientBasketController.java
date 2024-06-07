719b858b4e7508ac45cff6e9ea52aef80a238bec
Nomiya <s34656100@gmail.com>
2024-06-07 11:40:04 +0900

    Merge branch 'master' of https://yamashitasouta1215@github.com/yamashitasouta1215/yamashita.git

--- src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -2,6 +2,8 @@ package jp.co.sss.shop.controller.client.basket;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -10,5 +12,12 @@ public class ClientBasketController {
 	@Autowired
 	HttpSession session;
 	
-	//@RequestMapping(path ="")
+	//買い物かごタブをクリックした際のメソッド
+	//かご内の一覧表示
+	@RequestMapping(path ="client/basket/list", method = RequestMethod.GET)
+	public String ShowBasket() {
+		
+		@SuppressWarnings("unchecked")
+		
+	}
 }
