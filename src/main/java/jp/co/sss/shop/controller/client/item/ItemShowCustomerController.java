d6b1be2666a5e4eb63610568b279ac712106e1b5
UWAGAWA <aiponponponponpon@gmail.com>
2024-06-07 13:04:34 +0900

    商品一覧^^

--- src/main/java/jp/co/sss/shop/controller/client/item/ItemShowCustomerController.java
+++ src/main/java/jp/co/sss/shop/controller/client/item/ItemShowCustomerController.java
@@ -1,5 +1,24 @@
 package jp.co.sss.shop.controller.client.item;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sss.shop.repository.ItemRepository;
+
+@Controller
 public class ItemShowCustomerController {
+	
+	@Autowired
+	ItemRepository repository;
+	
+	@RequestMapping("/client/item/list/1")
+	public String showItemListByOrderById(Model model) {
+		model.addAttribute("items",repository.findAllByOrderById());
+		return "client/item/list";
+	}
+	
 
 }
--- src/main/java/jp/co/sss/shop/repository/ItemRepository.java
+++ src/main/java/jp/co/sss/shop/repository/ItemRepository.java
@@ -1,5 +1,7 @@
 package jp.co.sss.shop.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -42,4 +44,14 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
 	 * @return 商品エンティティ
 	 */
 	public Item findByNameAndDeleteFlag(String name, int notDeleted);
+	
+	List<Item> findAllByOrderByPriceDesc();
+
+	List<Item> findByPrice(Integer price);
+
+	List<Item> findByNameAndPrice(String name, Integer price);
+
+	List<Item> findByNameLike(String name);
+
+	List<Item> findAllByOrderById();
 }
