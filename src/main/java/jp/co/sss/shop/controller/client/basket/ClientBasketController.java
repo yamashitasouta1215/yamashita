a9a3794cce06ccafe856c6afda7033b5a89771ab
Nomiya <s34656100@gmail.com>
2024-06-10 11:17:00 +0900

    あああ

--- src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -26,15 +26,6 @@ public class ClientBasketController {
 	//かご内の一覧表示
 	@RequestMapping(path ="client/basket/list", method = RequestMethod.GET)
 	public String ShowBasket() {
-		
-		@SuppressWarnings("unchecked")
-		//セッションスコープに中身があるかの確認
-		List<BasketBean> basketItemList = (List<BasketBean>) session.getAttribute("basketItems");
-		if (basketItemList == null) {
-			basketItemList = new ArrayList<>();
-			session.setAttribute("basketItems", basketItemList);
-		}
-		
 		return "client/basket/list";
 	}
 	
@@ -43,36 +34,45 @@ public class ClientBasketController {
 	public String addBasket(Integer id, Model model) {
 		//セッションスコープ内にリスト要素がない場合、リストを作る
 		@SuppressWarnings("unchecked")
-		List<BasketBean> basketItemList = (List<BasketBean>) session.getAttribute("basketItems");
+		List<BasketBean> basketItemList = (List<BasketBean>) session.getAttribute("basketBeans");
 		if (basketItemList == null) {
 			basketItemList = new ArrayList<>();
 		}
 		
-		boolean existItemBasket = false;
-		BasketBean itemAddToBasket = null;
+		//ストックがいくつあるのか確認
+		Item itemStock =itemRepository.getReferenceById(id);
+		int itemStockNum = itemStock.getStock();
 		
-		//かご内に追加したい商品が存在する場合、その数を増やす
-		for (BasketBean itemInBasket : basketItemList) {
-			if (itemInBasket.getId() == id) {
-				itemAddToBasket = itemInBasket;
-				int newOrderNum = itemAddToBasket.getOrderNum() + 1;
-				itemAddToBasket.setOrderNum(newOrderNum);
-				existItemBasket = true;
+		if (0 < itemStockNum) {
+			//在庫がある場合
+			boolean existItemBasket = false;
+			BasketBean itemAddToBasket = null;
+			
+			//かご内に追加したい商品が存在する場合、その数を増やす
+			for (BasketBean itemInBasket : basketItemList) {
+				if (itemInBasket.getId() == id) {
+					itemAddToBasket = itemInBasket;
+					int newOrderNum = itemAddToBasket.getOrderNum() + 1;
+					itemAddToBasket.setOrderNum(newOrderNum);
+					existItemBasket = true;
+				}
 			}
-		}
-		
-		//かご内に追加したい商品が存在しない場合
-		if (!existItemBasket) {
-			Item item = itemRepository.getReferenceById(id);
-			itemAddToBasket = new BasketBean();
-			itemAddToBasket.setId(item.getId());
-			itemAddToBasket.setName(item.getName());
-			itemAddToBasket.setStock(item.getStock());
-			int i = 1;
-			int orderNum = i++;
-			itemAddToBasket.setOrderNum(orderNum);
-			//買い物かごリストに追加
-			basketItemList.add(itemAddToBasket);
+			
+			//かご内に追加したい商品が存在しない場合
+			if (!existItemBasket) {
+				Item item = itemRepository.getReferenceById(id);
+				itemAddToBasket = new BasketBean(item.getId(), item.getName(), item.getStock(), 1);
+				//買い物かごリストに追加
+				basketItemList.add(itemAddToBasket);
+			}
+			
+			//在庫が残り一個のとき
+			if (1 == itemStockNum) {
+				model.addAttribute("itemNameListLessThan", itemStock.getName());
+			}
+		} else {
+			//在庫がない場合
+			model.addAttribute("itemNameListZero", itemStock.getName());
 		}
 		
 		return "forward:/client/basket/list";
