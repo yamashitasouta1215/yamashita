5f25587f45dd3f710238c525f74fe8f3c08c42fe
Nomiya <s34656100@gmail.com>
2024-06-12 17:16:11 +0900

    a

--- src/main/java/jp/co/sss/shop/repository/ArtistRepository.java
+++ src/main/java/jp/co/sss/shop/repository/ArtistRepository.java
@@ -1,12 +1,8 @@
 package jp.co.sss.shop.repository;
 
-<<<<<<< HEAD
-import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-=======
->>>>>>> branch 'master' of https://yamashitasouta1215@github.com/yamashitasouta1215/yamashita.git
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
